ee018dd1bed576429a1d88d013d7e9d124cc19b9
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-20 18:26:00 +0200

    Changing placeholders

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = "https://mhutvjtzzhimwkqbgkpb.supabase.co";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1odXR2anR6emhpbXdrcWJna3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNDQ0ODksImV4cCI6MjA2MDcyMDQ4OX0.m-NuwKgy1KVUv44sALmUu6sLLW50eTVYyNkUlu8c6lQ";
+const url = "YOUR_PROJECT_URL";
+const key = "YOUR_ANON_KEY";
 
 export const supabase = createClient(url, key);
